f1b70e6c8b30d986c0c54a9687a35147137c4c76
BobWang001 <90195965+BobWang001@users.noreply.github.com>
2024-06-11 02:26:00 +0800

    Update definitions.cpp

--- definitions.cpp
+++ definitions.cpp
@@ -10,6 +10,9 @@ extern instruction* start;//属于全局的指令
 extern map<std::string, int>ins_num, cond_num;
 extern int tot_instructions;//总的指令数
 
+extern map<type_label, int>label_num;
+extern int tot_label;
+
 extern functions* func_head, * func_tail;
 extern map<type_label, int>map_function;
 extern int tot_functions;//总的函数数
