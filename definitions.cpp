b33c9d06fb90e493ddbc1e57149193bff2e2ca8c
BobWang001 <90195965+BobWang001@users.noreply.github.com>
2024-06-10 19:25:20 +0800

    Update definitions.cpp

--- definitions.cpp
+++ definitions.cpp
@@ -8,6 +8,8 @@ extern map<type_variables, int>map_global;//变量名到编号的映射
 
 extern instruction* start;//属于全局的指令
 extern map<std::string, int>ins_num;
+extern int tot_instructions;//总的指令数
 
 extern functions* func_head, * func_tail;
 extern map<type_label, int>map_function;
+extern int tot_functions;//总的函数数
